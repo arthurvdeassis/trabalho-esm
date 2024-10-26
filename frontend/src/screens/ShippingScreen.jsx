5d2738a421c7ac4005fdb5667e7f36b6d0b17c83
Arthur Vieira <arthurvdeassis@gmail.com>
2024-10-26 15:24:26 -0300

    frontend v1

---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { saveShippingAddress } from '../slices/cartSlice';
+
+const ShippingScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress.address || '');
+  const [city, setCity] = useState(shippingAddress.city || '');
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ''
+  );
+  const [country, setCountry] = useState(shippingAddress.country || '');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate('/payment');
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 />
+      <h1>Informações de entrega</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className='my-2' controlId='address'>
+          <Form.Label>Endereço</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Digite o endereço de entrega'
+            value={address}
+            required
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='city'>
+          <Form.Label>Cidade</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Digite a cidade correspondente'
+            value={city}
+            required
+            onChange={(e) => setCity(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='postalCode'>
+          <Form.Label>CEP</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Digite o CEP'
+            value={postalCode}
+            required
+            onChange={(e) => setPostalCode(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='country'>
+          <Form.Label>País</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Digite o país'
+            value={country}
+            required
+            onChange={(e) => setCountry(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='primary'>
+          Continuar
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default ShippingScreen;
